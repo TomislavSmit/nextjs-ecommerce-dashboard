f2227581bc66c20db3fb6dd78beafea235a25fc4
TomislavSmit <tomasmit@yahoo.com>
2025-03-06 12:54:00 +0100

    Fix header position

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -18,38 +18,30 @@ export default function DashboardLayout({
         }
     }, [loading, isAuthenticated, router])
 
-    if (loading) {
-        return (
-            <div className='flex items-center justify-center h-screen'>
-                Loading...
-            </div>
-        )
-    }
-
     return (
-        <div className='flex h-screen'>
-            <aside className='w-64 bg-gray-800 text-white p-4'>
-                <nav>
-                    <a href='/dashboard/categories' className='block py-2'>
-                        Categories
-                    </a>
-                    <a href='/dashboard/products' className='block py-2'>
-                        Products
-                    </a>
-                    <a href='/dashboard/users' className='block py-2'>
-                        Users
-                    </a>
-                </nav>
-            </aside>
-            <main className='flex-1 p-6'>
-                <header className='flex justify-between mb-4'>
-                    <div>Breadcrumb</div>
-                    <button onClick={logout} className='text-red-500'>
-                        Sign Out
-                    </button>
-                </header>
-                {children}
-            </main>
+        <div className='h-screen flex flex-col'>
+            <header className='flex justify-between items-center bg-gray-100 p-4 shadow'>
+                <div>Breadcrumb here</div>
+                <button onClick={logout} className='text-red-500'>
+                    Sign Out
+                </button>
+            </header>
+            <div className='flex flex-1'>
+                <aside className='w-64 bg-gray-800 text-white p-4'>
+                    <nav>
+                        <a href='/dashboard/categories' className='block py-2'>
+                            Categories
+                        </a>
+                        <a href='/dashboard/products' className='block py-2'>
+                            Products
+                        </a>
+                        <a href='/dashboard/users' className='block py-2'>
+                            Users
+                        </a>
+                    </nav>
+                </aside>
+                <main className='flex-1 p-6 overflow-auto'>{children}</main>
+            </div>
         </div>
     )
 }
