c3c41e7cf7424fe6266073deaadcf45b4b3277b3
TomislavSmit <tomasmit@yahoo.com>
2025-03-06 14:47:14 +0100

    Add metadata

--- src/app/dashboard/products/[id]/page.tsx
+++ src/app/dashboard/products/[id]/page.tsx
@@ -1,15 +1,43 @@
 import ProductDetailPage from '@/components/Products/ProductsDetailPage'
 import { API_URL } from '@/lib/api/config'
 import { Product } from '@/lib/types/products'
+import { Metadata } from 'next'
+import { notFound } from 'next/navigation'
 
-interface ProductDetailPageProps {
-    params: { id: string }
+type tParams = Promise<{ id: string }>
+
+export async function generateMetadata(props: {
+    params: tParams
+}): Promise<Metadata> {
+    const { id } = await props.params
+
+    if (!id) {
+        return {
+            title: 'Product Not Found',
+        }
+    }
+
+    const response = await fetch(`${API_URL}/products/${id}`)
+
+    if (!response.ok) {
+        notFound()
+    }
+
+    const product = await response.json()
+
+    return {
+        title: product.title,
+        description: product.description,
+        openGraph: {
+            title: product.title,
+            description: product.description,
+            images: [product.image],
+        },
+    }
 }
 
-export default async function ProductDetails({
-    params,
-}: ProductDetailPageProps) {
-    const { id } = await params
+export default async function ProductDetails(props: { params: tParams }) {
+    const { id } = await props.params
 
     if (!id) {
         return <div>Product ID not defined</div>
@@ -18,7 +46,7 @@ export default async function ProductDetails({
     const response = await fetch(`${API_URL}/products/${id}`)
 
     if (!response.ok) {
-        return <div>Product not found</div>
+        notFound()
     }
 
     const product: Product = await response.json()
--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,5 +1,11 @@
 import ProductsPage from '@/components/Products/ProductsPage'
 import { Product } from '@/lib/types/products'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'Products',
+    description: 'Browse all products in the store.',
+}
 
 export default async function Products() {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`)
